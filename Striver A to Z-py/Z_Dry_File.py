0969f6cd443d09695256109adc9d0043af0144d8
BlessenJoelG <154801802+BlessenJoelG@users.noreply.github.com>
2026-05-25 00:54:44 +0530

    recursion

--- Striver A to Z-py/Z_Dry_File.py
+++ Striver A to Z-py/Z_Dry_File.py
@@ -1,17 +1,1 @@
-#recursive approach ~ O(2^n) 
-class Solution:
-    def fib(self, n: int) -> int:
-        if n == 0:
-            return 0
-        elif n == 1:
-            return 1
-        else:
-            return self.fib(n-1)+self.fib(n-2)
-
-#iterative approach ~ O(n)       
-class Solution:
-    def fib(self, n: int) -> int:
-        a,b = 0,1
-        for i in range(n):
-            a,b = b,a+b
-        return a
+pass
