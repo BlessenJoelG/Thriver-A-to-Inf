9856c13cf15ded71a8d425b1320cbc002e9105d4
BlessenJoelG <154801802+BlessenJoelG@users.noreply.github.com>
2026-05-29 05:28:41 +0530

    arrays

--- Striver A to Z-py/3)Solve Problems on Arrays [Easy - Medium - Hard]/Largest Element.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def largestElement(self, nums):
-        maxi = 0
-        for i in range(len(nums)):
-            if maxi<nums[i]:
-                maxi = nums[i]
-        return maxi
--- Striver A to Z-py/Z_Dry_File.py
+++ Striver A to Z-py/Z_Dry_File.py
@@ -1,1 +1,9 @@
-pass
+nums = [8, 8, 7, 6, 5]
+nums.sort()
+largest = nums[len(nums)-1]
+seclargest = -1
+for i in range(len(nums)-1,-1,-1):
+    if nums[i]<largest:
+        seclargest = nums[i]
+        break
+print(seclargest)
