f8fd0569fde0e60428daa593a044be4933f8e342
BlessenJoelG <154801802+BlessenJoelG@users.noreply.github.com>
2026-05-29 22:54:13 +0530

    1)Things to Know in C++ or Java or Python or any language

---
+++ Striver A to Z-py/1)Learn_The_Basics/1)Things to Know in C++ or Java or Python or any language/Pass by Ref.py
@@ -0,0 +1,8 @@
+class Solution:
+    def reverse(self, arr: list) -> None:
+        l,r = 0,len(arr)-1
+        while(l<r):
+            arr[l],arr[r] = arr[r],arr[l]
+            l += 1
+            r -= 1
+        return arr
