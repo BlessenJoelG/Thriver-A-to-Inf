27cc975a4cf8e33c2264b50bc7fb0d4cf5ed613f
BlessenJoelG <154801802+BlessenJoelG@users.noreply.github.com>
2026-06-01 00:54:24 +0530

    arrays

--- Striver A to Z-py/Z_Dry_File.py
+++ Striver A to Z-py/Z_Dry_File.py
@@ -1,9 +1,6 @@
-nums = [8, 8, 7, 6, 5]
-nums.sort()
-largest = nums[len(nums)-1]
-seclargest = -1
-for i in range(len(nums)-1,-1,-1):
-    if nums[i]<largest:
-        seclargest = nums[i]
-        break
-print(seclargest)
+nums = [1, 2, 3, 4, 5]
+temp = [0]*len(nums)
+for i in range(1,len(nums)):
+    temp[i-1] = nums[i]
+temp[len(nums)-1] = nums[0]
+print(temp)
